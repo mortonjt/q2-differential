8271d0e39c99f4ad5f571e4ce03508169f7f465c
mortonjt <jamietmorton@gmail.com>
2021-07-01 15:42:08 -0600

    adding test skeleton

---
+++ q2_differential/tests/test_model.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestDEseq2(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class TestALDEx2(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
