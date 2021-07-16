779ae83788b82385668f2ca7d8e700893494cd98
mortonjt <jamietmorton@gmail.com>
2021-07-16 19:08:45 -0400

    adding plugin placeholder

---
+++ q2_differential/plugin_setup.py
@@ -0,0 +1,12 @@
+import qiime2.plugin
+import qiime2.sdk
+
+
+plugin = qiime2.plugin.Plugin(
+    name='differential',
+    version='0.0.1',
+    website="https://github.com/mortonjt/q2-differential",
+    citations=[],
+    short_description=('Plugin for differential abundance analysis.'),
+    description=(''),
+    package='q2-differential')
