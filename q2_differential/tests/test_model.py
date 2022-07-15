f8182279e1c3814cfea15ead325b589a45105f91
Dongmin Jin <djin@rustyamd2.cm.cluster>
2022-07-15 13:29:28 -0400

    modify test_model.py

--- q2_differential/tests/test_model.py
+++ q2_differential/tests/test_model.py
@@ -7,22 +7,7 @@ from q2_differential._model import DESeq2, SingleDESeq2, DiseaseSingle
 from birdman import ModelIterator
 from xarray.ufuncs import log2 as xlog
 import pandas.testing as pdt
-from q2_differential._stan.py import _case_control_negative_binomial_sim
-
-class TestDiseaseSingle(unitest.TestCase):
-    def setUp(self):
-        np.random.seed(0)
-        self.table, self.metadata, self.diff = _case_control_sim(
-            n=50, d=4, depth=100)
-        self.diff = clr(alr_inv(self.diff))
-
-    def test_stan_run(self):
-        models = ModelIterator(self.table, DiseaseSingle, metadata=self.metadata,
-                               category_column='Status', num_iter=128, num_warmup=1000)
-        for fid, m in models:
-            m.compile_model()
-            m.fit_model()
-            m.to_inference_object()
+from q2_differential._stan import _case_control_negative_binomial_sim
 
 class TestDEseq2(unittest.TestCase):
     def setUp(self):
@@ -52,6 +37,20 @@ class TestSingleDEseq2(unittest.TestCase):
             m.fit_model()
             m.to_inference_object()
 
+class TestDiseaseSingle(unittest.TestCase):
+    def setUp(self):
+        np.random.seed(0)
+        self.table, self.metadata, self.diff = _case_control_sim(
+            n=50, d=4, depth=100)
+        self.diff = clr(alr_inv(self.diff))
+
+    def test_stan_run(self):
+        models = ModelIterator(self.table, DiseaseSingle, metadata=self.metadata,
+                               category_column='Status', num_iter=128, num_warmup=1000)
+        for fid, m in models:
+            m.compile_model()
+            m.fit_model()
+            m.to_inference_object()
 
 if __name__ == '__main__':
     unittest.main()
