5908d057eec5875c2af9c982096779bf3ef014ad
Dongmin Jin <djin@rustyamd2.cm.cluster>
2022-07-15 14:25:23 -0400

    modify test_model.py

--- q2_differential/tests/test_model.py
+++ q2_differential/tests/test_model.py
@@ -9,33 +9,33 @@ from xarray.ufuncs import log2 as xlog
 import pandas.testing as pdt
 from q2_differential._stan import _case_control_negative_binomial_sim
 
-class TestDEseq2(unittest.TestCase):
-    def setUp(self):
-        self.table = biom.load_table(get_data_path('table.biom'))
-        self.metadata = pd.read_table(get_data_path('sample_metadata.txt'),
-                                      index_col=0)
-
-    def test_stan_run(self):
-        model = DESeq2(self.table, self.metadata, 'Status',
-                       num_iter=128, num_warmup=1000)
-        model.compile_model()
-        model.fit_model()
-        inf = model.to_inference_object()
-
-
-class TestSingleDEseq2(unittest.TestCase):
-    def setUp(self):
-        self.table = biom.load_table(get_data_path('table.biom'))
-        self.metadata = pd.read_table(get_data_path('sample_metadata.txt'),
-                                      index_col=0)
-
-    def test_stan_run(self):
-        models = ModelIterator(self.table, SingleDESeq2, metadata=self.metadata,
-                               category_column='Status', num_iter=128, num_warmup=1000)
-        for fid, m in models:
-            m.compile_model()
-            m.fit_model()
-            m.to_inference_object()
+#class TestDEseq2(unittest.TestCase):
+#    def setUp(self):
+#        self.table = biom.load_table(get_data_path('table.biom'))
+#        self.metadata = pd.read_table(get_data_path('sample_metadata.txt'),
+#                                      index_col=0)
+#
+#    def test_stan_run(self):
+#        model = DESeq2(self.table, self.metadata, 'Status',
+#                       num_iter=128, num_warmup=1000)
+#        model.compile_model()
+#        model.fit_model()
+#        inf = model.to_inference_object()
+#
+#
+#class TestSingleDEseq2(unittest.TestCase):
+#    def setUp(self):
+#        self.table = biom.load_table(get_data_path('table.biom'))
+#        self.metadata = pd.read_table(get_data_path('sample_metadata.txt'),
+#                                      index_col=0)
+#
+#    def test_stan_run(self):
+#        models = ModelIterator(self.table, SingleDESeq2, metadata=self.metadata,
+#                               category_column='Status', num_iter=128, num_warmup=1000)
+#        for fid, m in models:
+#            m.compile_model()
+#            m.fit_model()
+#            m.to_inference_object()
 
 class TestDiseaseSingle(unittest.TestCase):
     def setUp(self):
@@ -54,3 +54,4 @@ class TestDiseaseSingle(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
